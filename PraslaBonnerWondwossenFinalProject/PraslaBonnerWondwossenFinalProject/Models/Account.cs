521d9dc2b84225d3f3b12df846c225204a7e2bd1
jessicabonner <jessicabonner@utexas.edu>
2017-03-26 10:55:46 -0500

    Create Account.cs

---
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Models/Account.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace PraslaBonnerWondwossenFinalProject.Models
+{
+    public enum AccountTypes { Saving, Checking, IRA, Stock Portfolio }
+    public class Account
+
+    {        
+        //primary key
+        public Int32 AccountID { get; set; }
+        
+        public static DateTime Today { get; }
+        
+        [Required(ErrorMessage = "You must select an account type")]
+        [Display(Name="Account Type")]
+        public AccountTypes Type { get; set; }
+        
+        [Display(Name="Account Name")]
+        public Int32 Name { get; set; }
+        
+        [Required(ErrorMessage= "Please enter an initial deposit")]
+        [Display(Name="Initial Deposit")]
+        public Decimal Balance { get; set; }
+        
+        public virtual Customer Customer { get; set; }
+    }
