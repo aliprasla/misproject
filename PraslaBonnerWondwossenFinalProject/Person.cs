cb22dd29ef455ce486972e77b590c179aca83500
NateWondwossen <natewondwossen@utexas.edu>
2017-03-30 09:00:09 -0700

    Add files via upload
    Person Class

---
+++ PraslaBonnerWondwossenFinalProject/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LonghornBank.Models
+{
+    public class Person
+    {
+        public int CustomerID { get; set; }
+        
+        [Required]
+        public string FName { get; set; }
+
+        [Required]
+        public string LName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        public bool? isCustomer { get; set; }
+
+        public bool? isEmployee { get; set; }
+
+        public bool? isManager { get; set; }
+
+        public bool? isDeactive { get; set; }
+    }
+}
