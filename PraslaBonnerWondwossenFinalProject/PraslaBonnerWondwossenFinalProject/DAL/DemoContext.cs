8a6e512bd6480d85d10c7efbad4aa9a848132326
DESKTOP-S6A4V95\MSBStudent <natewondwossen@utexas.edu>
2017-03-31 10:10:27 -0700

    Updated Web.Config file with connection string. Add DAL folder with db contexts

---
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/DAL/DemoContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PraslaBonnerWondwossenFinalProject.Models;
+using System.Data.Entity;
+
+
+namespace PraslaBonnerWondwossenFinalProject.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("MyDbContext") { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Dispute> Disputes { get; set; }
+        public DbSet<Payee> Payees { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        
+    }
+}
