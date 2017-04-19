e6810ddaac7bab2fc5f27b2eccfc5039f5a99e9c
JESSICA\Jessica <jessicabonner@utexas.edu>
2017-04-19 14:33:24 -0500

    stuff

--- PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Models/Payee.cs
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Models/Payee.cs
@@ -1,94 +1,47 @@
-﻿
 using System;
-
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Web;
-
 using System.ComponentModel.DataAnnotations;
 
 
-
-
-
 namespace PraslaBonnerWondwossenFinalProject.Models
-
 {
-
     public enum PayeeTypes { CreditCard, Utilities, Rent, Mortgage, Other }
-
     public class Payee
 
-
-
     {
-
         //primary key
-
         public Int32 PayeeID { get; set; }
 
-
-
         [Required(ErrorMessage = "Name required")]
-
         [Display(Name = "Account Name")]
-
         public String Name { get; set; }
 
-
-
         [Required(ErrorMessage = "Street Required")]
-
         [Display(Name = "Street")]
-
         public String Street { get; set; }
 
-
-
         [Required(ErrorMessage = "City Required")]
-
         [Display(Name = "City")]
-
         public String City { get; set; }
 
-
-
         [Required(ErrorMessage = "State Required")]
-
         [Display(Name = "State")]
-
         public String State { get; set; }
 
-
-
         [Required(ErrorMessage = "Zip Code Required")]
-
         [Display(Name = "Zip Code")]
-
         public Int32 ZipCode { get; set; }
 
-
-
         [Required(ErrorMessage = "Phone Number Required")]
-
         [Display(Name = "Phone Number")]
-
         public Int32 Phone { get; set; }
 
-
-
         [Required(ErrorMessage = "Type Required")]
-
         [Display(Name = "Type")]
-
         public PayeeTypes Type { get; set; }
 
 
-
-
-
     }
-
 }
--- PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Models/Transaction.cs
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Models/Transaction.cs
@@ -1,56 +1,28 @@
-﻿
-using System;
-
+﻿using System;
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Web;
-
 using System.ComponentModel.DataAnnotations;
 
 
-
-
-
 namespace PraslaBonnerWondwossenFinalProject.Models
-
 {
-
     // we may need to add more types here
-
     public enum TransactionTypes { Deposit, Transfer, Withdrawal }
-
     public class Transaction
 
-
-
     {
-
         //primary key
-
         public int TransactionID { get; set; }
-
-        [Required(ErrorMessage = "Date is Required")]
-
+        [Required(ErrorMessage ="Date is Required")]
         public DateTime Date { get; set; }
-
         [Required(ErrorMessage = "Select a valid Transaction Type")]
-
         public TransactionTypes Type { get; set; }
-
         [Required(ErrorMessage = "Enter a valid Tranaction Amount")]
-
         public Decimal Amount { get; set; }
-
         public String Description { get; set; }
-
         //define one to many relationships - One customer can have many transactions - need a customer class
-
-        public virtual AppUser Customer { get; set; }
-
+        public virtual Person Customer { get; set; }
         public virtual Dispute Dispute { get; set; }
-
     }
-
 }
