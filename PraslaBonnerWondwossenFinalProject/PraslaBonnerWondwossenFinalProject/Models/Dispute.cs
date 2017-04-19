d21eecbb918d1e4a19caea3857d5b86b65e47dea
aprasla0922 <aprasla0922@gmail.com>
2017-04-19 16:20:39 -0500

    RESET MOTHERFUCKERS <- Identity works

--- PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Models/Dispute.cs
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Models/Dispute.cs
@@ -1,25 +1,50 @@
-﻿using System;
+﻿
+using System;
+
 using System.Collections.Generic;
+
 using System.ComponentModel.DataAnnotations;
+
 using System.Linq;
+
 using System.Web;
 
+
+
 namespace PraslaBonnerWondwossenFinalProject.Models
+
 {
-    public enum Status {WaitingOnManager,Approved,NotApproved }
+
+    public enum Status { WaitingOnManager, Approved, NotApproved }
+
     public class Dispute
+
     {
+
         public int DisputeID { get; set; }
+
         [Required]
+
         public Status Status { get; set; }
+
         [Display(Name = "Transaction Dispute Description")]
+
         [Required(ErrorMessage = "You must describe the dispute")]
+
         public string CustomerDescription { get; set; }
+
         [Display(Name = "Manager Comments on the dispute")]
+
         public string ManagerDescription { get; set; }
-        public virtual Person AssignedManager { get; set; }
+
+        public virtual AppUser AssignedManager { get; set; }
+
         [Required(ErrorMessage = "Dispute must be attached to a specific transaction")]
-        public virtual Transaction Transaction {get; set;}
+
+        public virtual Transaction Transaction { get; set; }
+
+
 
     }
+
 }
---
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Models/Product.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+
+
+
+namespace PraslaBonnerWondwossenFinalProject.Models
+
+{
+
+    public enum ProductTypes { Saving, Checking, IRA, StockPortfolio }
+
+    public class Product
+
+
+
+    {
+
+        //primary key
+
+        public Int32 ProductID { get; set; }
+
+        
+
+        private Int32 _decProductNumber { get; set; }
+
+        public Int32 ProductNumber
+
+        {
+
+            get { return _decProductNumber; }
+
+            set { _decProductNumber = Convert.ToInt32(9999999999 + ProductID); }
+
+        }
+
+  
+
+        public static DateTime Today { get; }
+
+
+
+        [Required(ErrorMessage = "You must select an Product type")]
+
+        [Display(Name = "Product Type")]
+
+        public ProductTypes Type { get; set; }
+
+
+
+        [Display(Name = "Product Name")]
+
+        public String Name { get; set; }
+
+
+
+        [Required(ErrorMessage = "Please enter an initial deposit")]
+
+        [Display(Name = "Initial Deposit")]
+
+        public Decimal Balance { get; set; }
+
+
+
+        public virtual AppUser Customer { get; set; }
+
+
+        
+
+        public virtual AppUser Person { get; set; }
+
+    }
+
+}
