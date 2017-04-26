44b866db5b132b5ace1f95fd3e5dc12c1cd01728
DESKTOP-S6A4V95\MSBStudent <natewondwossen@utexas.edu>
2017-04-25 19:49:54 -0700

    Merge branch 'master' of https://www.github.com/aprasla0922/misproject
    # Conflicts:
    #	PraslaBonnerWondwossenFinalProject/.vs/config/applicationhost.config
    #	PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache

---
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Controllers/DisputesController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PraslaBonnerWondwossenFinalProject.Models;
+
+namespace PraslaBonnerWondwossenFinalProject.Controllers
+{
+    public enum Status { Resolved, NotApproved, Approved, WaitingOnManager }
+    public class DisputesController : Controller
+    {
+
+
+        private AppDbContext db = new AppDbContext();
+        // GET: Disputes
+        public ActionResult Index()
+        {
+            var query = from d in db.Disputes
+                        select d;
+
+            query = query.Where(d => d.Status == Status.WaitingOnManager);
+            List<Dispute> SelectedDisuputes = query.ToList();
+
+            return View("Index", SelectedDisuputes);
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View("Index", db.Disputes.ToList());
+        }
+
+        public ActionResult Resolve(string Id)
+        {
+            if (Id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dispute dispute = db.Disputes.Find(Id);
+            if (dispute == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dispute);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Resolve([Bind(Include = "DisputeId")]Status SelectedResponse, String commentString, int AdjustedAmount, Dispute dispute)
+        {
+            if (ModelState.IsValid)
+            {
+                Dispute disputeToChange = db.Disputes.Find(dispute.DisputeID);
+
+                //If manager comments
+                if (commentString != null)
+                {
+                    disputeToChange.ManagerDescription = commentString;
+                }
+                //If manager adjusts amount
+                if (AdjustedAmount != 0)
+                {
+                    disputeToChange.Transaction.Amount = AdjustedAmount;
+                    disputeToChange.Status = Status.Resolved;
+                }
+                if (SelectedResponse == Status.Approved)
+                {
+                    disputeToChange.Transaction.Amount = disputeToChange.DisputeAmount;
+                    disputeToChange.Status = Status.Resolved;
+                }
+                if (SelectedResponse == Status.NotApproved)
+                {
+                    disputeToChange.Status = Status.Resolved;
+                }
+                db.Entry(disputeToChange).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(dispute);
+        }
+    }
+}
---
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Controllers/ManagersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PraslaBonnerWondwossenFinalProject.Models;
+
+namespace PraslaBonnerWondwossenFinalProject.Controllers
+{
+    public class ManagersController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+        // GET: Managers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
