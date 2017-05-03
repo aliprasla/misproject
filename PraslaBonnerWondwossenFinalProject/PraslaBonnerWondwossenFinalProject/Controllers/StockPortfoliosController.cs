ba76367d0a0796dfec8fe56c48dc233d96fd5234
JESSICA\Jessica <jessicabonner@utexas.edu>
2017-05-02 20:46:56 -0500

    Customer can now view details of stock portfolio (well, most of the details) Error page if customer tries to make more than one stock portfolio

--- PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Controllers/StockPortfoliosController.cs
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Controllers/StockPortfoliosController.cs
@@ -13,6 +13,10 @@ namespace PraslaBonnerWondwossenFinalProject.Controllers
     {
         private AppDbContext db = new AppDbContext();
 
+        public ActionResult Error()
+        {
+            return View();
+        }
         // GET: StockPortfolios
         public ActionResult Index()
         {
@@ -30,7 +34,7 @@ namespace PraslaBonnerWondwossenFinalProject.Controllers
             {
                 return HttpNotFound();
             }
-            //View All Transactions associated with account
+            
 
             return View(StockPortfolio);
         }
@@ -38,7 +42,7 @@ namespace PraslaBonnerWondwossenFinalProject.Controllers
         public ActionResult Create()
         {
             AppUser person = db.Users.Find(User.Identity.GetUserId());
-            if (person.isActive == true || person.isActive == false || person.isActive == null)
+            if (person.isActive == true)
             {
                 return View();
             }
@@ -51,45 +55,55 @@ namespace PraslaBonnerWondwossenFinalProject.Controllers
         [HttpPost]
         public ActionResult Create([Bind(Include = "BankAccountID")]StockPortfolio StockPortfolio)
         {
-            StockPortfolio.Type = AccountTypes.Stock;
-            StockPortfolio.Name = "Longorn Stock";
-            StockPortfolio.Balance = 0;
-            StockPortfolio.Customer = db.Users.Find(User.Identity.GetUserId());
-            StockPortfolio.isBalanced = false;
-            StockPortfolio.isApproved = false;
-            StockPortfolio.CashBalance = 0;
-            StockPortfolio.Gains = 0;
-            StockPortfolio.Fees = 0;
-            StockPortfolio.Bonuses = 0;
-            db.Users.Find(User.Identity.GetUserId()).BankAccounts.Add(StockPortfolio);
-            db.SaveChanges();
+            AppUser user = db.Users.Find(User.Identity.GetUserId());
+            if (user.StockPortfolio == null) 
+                {
+                StockPortfolio.Type = AccountTypes.Stock;
+                StockPortfolio.Name = "Longorn Stock";
+                StockPortfolio.Balance = 0;
+                StockPortfolio.Customer = db.Users.Find(User.Identity.GetUserId());
+                StockPortfolio.isBalanced = false;
+                StockPortfolio.isApproved = false;
+                StockPortfolio.CashBalance = 0;
+                StockPortfolio.Gains = 0;
+                StockPortfolio.Fees = 0;
+                StockPortfolio.Bonuses = 0;
+                //StockPortfolio.AccountNumber = Convert.ToInt32((10000000000 + StockPortfolio.BankAccountID));
+                db.Users.Find(User.Identity.GetUserId()).BankAccounts.Add(StockPortfolio);
+                db.Users.Find(User.Identity.GetUserId()).StockPortfolio = StockPortfolio;
 
-            
-            
-
-            if (ModelState.IsValid)
-            {
-                var item = db.BankAccounts.OrderByDescending(i => i.AccountNumber).FirstOrDefault();
-                StockPortfolio.AccountNumber = item.AccountNumber + 1;
-
-
-                //create a dispute for manager approval
-                Dispute now = new Dispute();
-                now.Status = Status.WaitingOnManager;
-                now.CustomerDescription = "Customer " + User.Identity.Name + "has applied for a stock portfolio. Please approve or deny this deposit.";
-                now.DisputeAmount = 0;
-                db.Disputes.Add(now);
-
-                //AppUser current = db.Users.Find(User.Identity.GetUserId());
-                //StockPortfolio.Customer = current;
-                //current.StockPortfolio = (StockPortfolio);
-                //db.StockPortfolios.Add(StockPortfolio);
                 db.SaveChanges();
 
-                return RedirectToAction("Index","Customers");
 
+
+
+                if (ModelState.IsValid)
+                {
+                    var item = db.BankAccounts.OrderByDescending(i => i.AccountNumber).FirstOrDefault();
+                    StockPortfolio.AccountNumber = item.AccountNumber + 1;
+
+
+                    //TODO:create a dispute for manager approval
+                    //Dispute now = new Dispute();
+                    //now.Status = Status.WaitingOnManager;
+                    //now.CustomerDescription = "Customer " + User.Identity.Name + "has applied for a stock portfolio. Please approve or deny this deposit.";
+                    //now.DisputeAmount = 0;
+                    //db.Disputes.Add(now);
+
+
+
+
+                    db.SaveChanges();
+
+
+                    return RedirectToAction("Index", "Customers");
+
+                }
             }
-
+            else
+            {
+                return RedirectToAction("Error");
+            }
 
 
             return View();
