10d3df38768eb867fc4aeb518d0444cf24b7a413
JESSICA\Jessica <jessicabonner@utexas.edu>
2017-05-04 23:04:28 -0500

    selling stocks

---
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Controllers/SalesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PraslaBonnerWondwossenFinalProject.Models;
+
+namespace PraslaBonnerWondwossenFinalProject.Controllers
+{
+    public class SalesController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Sales
+        public ActionResult Index()
+        {
+            return View(db.Sales.ToList());
+        }
+
+        // GET: Sales/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sales sales = db.Sales.Find(id);
+            if (sales == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sales);
+        }
+
+        // GET: Sales/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sales/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SalesId,Shares,date,NetProfit,SharesLeft,PurchaseId")] Sales sales)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Sales.Add(sales);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sales);
+        }
+
+        // GET: Sales/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sales sales = db.Sales.Find(id);
+            if (sales == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sales);
+        }
+
+        // POST: Sales/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SalesId,Shares,date,NetProfit,SharesLeft,PurchaseId")] Sales sales)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sales).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sales);
+        }
+
+        // GET: Sales/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sales sales = db.Sales.Find(id);
+            if (sales == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sales);
+        }
+
+        // POST: Sales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sales sales = db.Sales.Find(id);
+            db.Sales.Remove(sales);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
