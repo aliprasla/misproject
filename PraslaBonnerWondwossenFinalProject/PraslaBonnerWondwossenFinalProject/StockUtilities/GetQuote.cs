bfabf6cbbb035096e93bf22be767b88edc9693a5
DESKTOP-S6A4V95\MSBStudent <natewondwossen@utexas.edu>
2017-05-04 20:34:18 -0700

    stock stuff

--- PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/StockUtilities/GetQuote.cs
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/StockUtilities/GetQuote.cs
@@ -119,6 +119,117 @@ namespace PraslaBonnerWondwossenFinalProject.StockUtilities
 
         }
 
+        public static StockQuote GetStock(String symbol, DateTime date)
+
+        {
+
+            //http://finance.yahoo.com/q/hp?s=WU&a=01&b=19&c=2010&d=01&e=19&f=2010&g=d
+
+
+
+            String month = (date.Month - 1).ToString();
+
+            String day = date.Day.ToString();
+
+            String year = date.Year.ToString();
+
+
+
+            string baseURL = "http://ichart.finance.yahoo.com/table.csv?s={0}&a={1}&b={2}&c={3}&d={4}&e={5}&f={6}&g=d&ignore=.csv&h=npl1v";
+
+            //http://ichart.finance.yahoo.com/table.csv?s=WU&a=01&b=19&c=2010&d=01&e=19&f=2010&g=d&ignore=.csv&h=pl1vn
+
+
+
+
+
+            string url = string.Format(baseURL, symbol, month, day, year, month, day, year);
+
+
+
+            //Get page showing the table with the chosen indices
+
+            System.Net.HttpWebRequest request = null;
+
+
+
+            Console.WriteLine(url);
+
+
+
+            //csv content
+
+            string docText = string.Empty;
+
+            StockQuote stock = null;
+
+            try
+
+            {
+
+                request = (HttpWebRequest)WebRequest.CreateDefault(new Uri(url));
+
+                request.Timeout = 300000;
+
+
+
+                using (var response = (HttpWebResponse)request.GetResponse())
+
+                using (StreamReader stReader = new StreamReader(response.GetResponseStream()))
+
+                {
+
+                    string output = stReader.ReadLine();
+
+                    //go to line with actual data
+
+                    output = stReader.ReadLine();
+
+                    //output = stReader.ReadLine();
+
+                    //Date,Open,High,Low,Close,Volume,Adj Close
+
+                    //2010 - 02 - 19,16.389999,16.459999,16.27,16.35,6963200,13.508243
+
+
+
+
+
+                    string[] sa = output.Split(new char[] { ',' });
+
+
+
+                    stock = new StockQuote();
+
+                    stock.Symbol = symbol;
+
+                    stock.Name = GetStock(symbol).Name;
+
+                    stock.PreviousClose = double.Parse(sa[4]);
+
+                    stock.LastTradePrice = double.Parse(sa[6]);
+
+                    stock.Volume = double.Parse(sa[5]);
+
+                }
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                //Throw some exception here.
+
+            }
+
+            return stock;
+
+
+
+        }
+
+
     }
 
 }
