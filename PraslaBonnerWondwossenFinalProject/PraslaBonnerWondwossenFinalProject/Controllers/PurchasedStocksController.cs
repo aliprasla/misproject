c917fe3d2887615cd39c9a6184fbbf57f461d29e
JESSICA\Jessica <jessicabonner@utexas.edu>
2017-05-04 22:34:27 -0500

    shit

--- PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Controllers/PurchasedStocksController.cs
+++ PraslaBonnerWondwossenFinalProject/PraslaBonnerWondwossenFinalProject/Controllers/PurchasedStocksController.cs
@@ -11,6 +11,8 @@ using Microsoft.AspNet.Identity;
 
 namespace PraslaBonnerWondwossenFinalProject.Controllers
 {
+
+
     public class PurchasedStocksController : Controller
     {
         private AppDbContext db = new AppDbContext();
@@ -18,7 +20,9 @@ namespace PraslaBonnerWondwossenFinalProject.Controllers
         // GET: PurchasedStocks
         public ActionResult Index()
         {
+
             AppUser customer = db.Users.Find(User.Identity.GetUserId());
+            if(customer==null) { return View("Login", "Account"); }
             return View(customer.StockPortfolio.purchasedstocks.ToList());
         }
 
@@ -37,93 +41,98 @@ namespace PraslaBonnerWondwossenFinalProject.Controllers
             return View(purchasedStock);
         }
 
-        // GET: PurchasedStocks/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: PurchasedStocks/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "PurchasedStockId,Shares,InitialPrice,TotalFees")] PurchasedStock purchasedStock)
-        {
-            if (ModelState.IsValid)
-            {
-                db.PurchasedStocks.Add(purchasedStock);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
 
-            return View(purchasedStock);
-        }
+        
 
-        // GET: PurchasedStocks/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PurchasedStock purchasedStock = db.PurchasedStocks.Find(id);
-            if (purchasedStock == null)
-            {
-                return HttpNotFound();
-            }
-            return View(purchasedStock);
-        }
 
-        // POST: PurchasedStocks/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "PurchasedStockId,Shares,InitialPrice,TotalFees")] PurchasedStock purchasedStock)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(purchasedStock).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(purchasedStock);
-        }
+        //// GET: PurchasedStocks/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // GET: PurchasedStocks/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PurchasedStock purchasedStock = db.PurchasedStocks.Find(id);
-            if (purchasedStock == null)
-            {
-                return HttpNotFound();
-            }
-            return View(purchasedStock);
-        }
+        //// POST: PurchasedStocks/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "PurchasedStockId,Shares,InitialPrice,TotalFees")] PurchasedStock purchasedStock)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.PurchasedStocks.Add(purchasedStock);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
 
-        // POST: PurchasedStocks/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            PurchasedStock purchasedStock = db.PurchasedStocks.Find(id);
-            db.PurchasedStocks.Remove(purchasedStock);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //    return View(purchasedStock);
+        //}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //// GET: PurchasedStocks/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    PurchasedStock purchasedStock = db.PurchasedStocks.Find(id);
+        //    if (purchasedStock == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(purchasedStock);
+        //}
+
+        //// POST: PurchasedStocks/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "PurchasedStockId,Shares,InitialPrice,TotalFees")] PurchasedStock purchasedStock)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(purchasedStock).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(purchasedStock);
+        //}
+
+        //// GET: PurchasedStocks/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    PurchasedStock purchasedStock = db.PurchasedStocks.Find(id);
+        //    if (purchasedStock == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(purchasedStock);
+        //}
+
+        //// POST: PurchasedStocks/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    PurchasedStock purchasedStock = db.PurchasedStocks.Find(id);
+        //    db.PurchasedStocks.Remove(purchasedStock);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
     }
 }
